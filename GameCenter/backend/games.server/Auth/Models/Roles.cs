004ca4893f81e78695c018ac7ce09ccbb4820cf2
alexxeyy <phoenix332228@gmail.com>
2025-05-16 03:54:11 +0300

    new model *roles*

---
+++ GameCenter/backend/games.server/Auth/Models/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace Auth.Models
+{
+    public class Roles
+    {
+        public string Admin { get; set; }
+        public string Moderator { get; set; }
+        public string User { get; set; }
+    }
+}
