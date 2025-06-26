2c164ddca78c645e2158515f520e97c1f6d53332
alexxeyy <phoenix332228@gmail.com>
2025-06-26 18:52:52 +0300

    new

--- backend/games-center.server/auth/AuthAPI/Data/AuthDbContext.cs
+++ backend/games-center.server/auth/AuthAPI/Data/AuthDbContext.cs
@@ -6,10 +6,8 @@ namespace AuthAPI.Data
     public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-
+            : base(options) { }
+        
         public DbSet<User> Users { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
