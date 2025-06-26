6e388ee45a4e1cff90400ad7e0ef9a9c18c7a829
alexxeyy <phoenix332228@gmail.com>
2025-06-26 19:35:24 +0300

    new model for roles

---
+++ backend/games-center.server/auth/AuthAPI/Data/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthAPI.Data
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Moderator = "Moderator";
+    }
+}
