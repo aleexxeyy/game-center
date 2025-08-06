326f970cbf4fe00741aa547e9551642537cdbc83
alexxeyy <phoenix332228@gmail.com>
2025-08-06 18:42:55 +0300

    updated domen models

--- backend/games-center.server/auth/AuthAPI/Models/RolePermission.cs
+++ backend/games-center.server/auth/AuthAPI/Models/RolePermission.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace AuthAPI.Models;
 
 public class RolePermission
 {
-    
+    public string RoleId { get; set; } = null!;
+    public IdentityRole Role { get; set; } = null!;
+    public Guid PermissionId { get; set; }
+    public Permission Permission { get; set; } = null!;
 }
