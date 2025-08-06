0dffd517d870087e0342b1f5cef3e28b5aca025c
alexxeyy <phoenix332228@gmail.com>
2025-08-06 18:41:23 +0300

    new folder and files extensions auth and jwt

--- backend/games-center.server/auth/AuthAPI/Extensions/AuthBuilderExtensions.cs
+++ backend/games-center.server/auth/AuthAPI/Extensions/AuthBuilderExtensions.cs
@@ -1,6 +1,33 @@
+using AuthAPI.Configurations;
+using AuthAPI.Data;
+using AuthAPI.Middlewares;
+using Microsoft.AspNetCore.Identity;
+
 namespace AuthAPI.Extensions;
 
-public class AuthBuilderExtensions
+public static class AuthBuilderExtensions
 {
-    
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        
+        // Инициализация ролей и разрешений...
+    }
+
+    public static void ConfigureMiddlewares(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseMiddleware<ErrorHandlerMiddleware>();
+        app.UseMiddleware<AuthMiddleware>();
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
 }
--- backend/games-center.server/auth/AuthAPI/Extensions/ServiceCollectionExtensions.cs
+++ backend/games-center.server/auth/AuthAPI/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,13 @@
+using AuthAPI.Configurations;
+using AuthAPI.Services;
+
 namespace AuthAPI.Extensions;
 
-public class ServiceCollectionExtensions
+public static class ServiceCollectionExtensions
 {
-    
+    public static void ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IRoleService, RoleService>();
+    }
 }
