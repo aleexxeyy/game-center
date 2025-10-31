dafb9084bf88e79e7a62e45f017226ec3ab98f1b
alexxeyy <phoenix332228@gmail.com>
2025-10-31 20:44:23 +0300

    add logic login/register user

--- backend/games-center.server/Auth.API/Services/AuthService.cs
+++ backend/games-center.server/Auth.API/Services/AuthService.cs
@@ -1,6 +1,39 @@
+using Auth.API.Models;
+using Auth.API.Repositories;
+using Microsoft.AspNetCore.Identity;
+
 namespace Auth.API.Services;
 
-public class AuthService
+public class AuthService(IAuthRepository repository, IPasswordHasher<User> passwordHasher) : IAuthService
 {
-    
+    private IAuthRepository _repository = repository;
+    private IPasswordHasher<User> _passwordHasher = passwordHasher;
+
+
+    public Task<User> RegisterAsync(RegisterModel model)
+    {
+        var exsistingUser = _repository.GetUserByUserName(model.UserName);
+
+        if (exsistingUser is not null)
+            throw new Exception("User is already user name is exist");
+
+        var newUser = _repository.CreateUser(model);
+
+        return newUser;
+    }
+
+    public async Task<User> LoginAsync(LoginModel model)
+    {
+        var user = await _repository.GetUserByUserName(model.UserName);
+
+        if (user is null)
+            throw new KeyNotFoundException("User not found");
+
+        var passwordHash = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, model.Password);
+
+        if (passwordHash == PasswordVerificationResult.Failed)
+            throw new UnauthorizedAccessException("Password is not correct");
+
+        return user;
+    }
 }
--- backend/games-center.server/Auth.API/Services/IAuthService.cs
+++ backend/games-center.server/Auth.API/Services/IAuthService.cs
@@ -1,6 +1,9 @@
+using Auth.API.Models;
+
 namespace Auth.API.Services;
 
 public interface IAuthService
 {
-    
+    Task<User> RegisterAsync(RegisterModel model);
+    Task<User> LoginAsync(LoginModel model);
 }
