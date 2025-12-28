f1f553748990b3f6c63f5f0af181cb4352ecae0a
tk_ctx_switch <phoenix332228@gmail.com>
2025-12-28 21:04:21 +0300

    Delete backend/games-center.server/Auth.API directory

--- backend/games-center.server/Auth.API/DataAccess/AuthDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Auth.API.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace Auth.API.DataAccess;
-
-public class AuthDbContext(DbContextOptions<AuthDbContext> options) : DbContext(options)
-{
-    public DbSet<User> Users { get; set; }
-}
